4b3d3185a71345a55db5ef2fc3e6120fcc9f19dc
Antonio Martínez <nonomartinezlopez2004@gmail.com>
2024-03-12 18:28:46 +0100

    initial commit

---
+++ api/CofradiasAPI/src/main/java/com/nonomartinez/sfc/cofradiasapi/CofradiasApiApplication.java
@@ -0,0 +1,13 @@
+package com.nonomartinez.sfc.cofradiasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CofradiasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CofradiasApiApplication.class, args);
+	}
+
+}
---
+++ api/CofradiasAPI/src/test/java/com/nonomartinez/sfc/cofradiasapi/CofradiasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nonomartinez.sfc.cofradiasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CofradiasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
