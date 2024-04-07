fcbdcd4543011d93eb9e1a0ae3560efc353ff3ac
Antonio Martinez <nonomartinezlopez2004@gmail.com>
2024-04-07 17:38:27 +0200

    Âfuncionalidad agregada correctamente

--- api/CofradiasAPI/src/main/java/com/nonomartinez/sfc/cofradiasapi/card/controller/CardController.java
+++ api/CofradiasAPI/src/main/java/com/nonomartinez/sfc/cofradiasapi/card/controller/CardController.java
@@ -1,12 +1,27 @@
 package com.nonomartinez.sfc.cofradiasapi.card.controller;
 
+import com.nonomartinez.sfc.cofradiasapi.MyPage;
+import com.nonomartinez.sfc.cofradiasapi.card.dto.GetCardDTO;
 import com.nonomartinez.sfc.cofradiasapi.card.service.CardService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/cards")
 public class CardController {
 
     private final CardService cardService;
+
+    @GetMapping("/user/{id}")
+    public MyPage<GetCardDTO> getCardsUser (@PathVariable UUID id, Pageable pageable){
+        return cardService.getCardsUser(id, pageable);
+    }
 }
