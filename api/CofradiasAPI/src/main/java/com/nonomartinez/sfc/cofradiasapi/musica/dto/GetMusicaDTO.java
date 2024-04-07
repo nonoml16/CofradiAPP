901bf296f304935ef41c69be35d1919808af7a8b
Antonio Martinez <nonomartinezlopez2004@gmail.com>
2024-04-07 19:17:35 +0200

    funcionalidad agregada correctamente

--- api/CofradiasAPI/src/main/java/com/nonomartinez/sfc/cofradiasapi/musica/dto/GetMusicaDTO.java
+++ api/CofradiasAPI/src/main/java/com/nonomartinez/sfc/cofradiasapi/musica/dto/GetMusicaDTO.java
@@ -1,13 +1,37 @@
 package com.nonomartinez.sfc.cofradiasapi.musica.dto;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import com.nonomartinez.sfc.cofradiasapi.hermandad.dto.GetHermandadDTO;
+import com.nonomartinez.sfc.cofradiasapi.hermandad.model.Hermandad;
+import com.nonomartinez.sfc.cofradiasapi.musica.model.Musica;
+import com.nonomartinez.sfc.cofradiasapi.musica.views.MusicaViews;
+import com.nonomartinez.sfc.cofradiasapi.paso.model.Paso;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public record GetMusicaDTO(
+        @JsonView({MusicaViews.MusicaDetails.class})
         String nombre,
-        String anno,
+        @JsonView({MusicaViews.MusicaDetails.class})
+        int anno,
+        @JsonView({MusicaViews.MusicaDetails.class})
         String localidad,
+        @JsonView({MusicaViews.MusicaDetails.class})
         List<GetHermandadDTO> hermandades
 ) {
+    public static GetMusicaDTO of(Musica m){
+        List<Hermandad> hermandades = new ArrayList<>();
+        for (Paso paso:
+                m.getPasos()) {
+            hermandades.add(paso.getHermandad());
+
+        }
+        return new GetMusicaDTO(
+                m.getNombre(),
+                m.getAnnoFundacion(),
+                m.getLocalidad(),
+                hermandades.stream().map(GetHermandadDTO::of).toList()
+        );
+    }
 }
---
+++ api/CofradiasAPI/src/main/java/com/nonomartinez/sfc/cofradiasapi/musica/views/MusicaViews.java
@@ -0,0 +1,7 @@
+package com.nonomartinez.sfc.cofradiasapi.musica.views;
+
+import com.nonomartinez.sfc.cofradiasapi.hermandad.views.HermandadViews;
+
+public class MusicaViews {
+    public static class MusicaDetails extends HermandadViews.HermandadList {}
+}
