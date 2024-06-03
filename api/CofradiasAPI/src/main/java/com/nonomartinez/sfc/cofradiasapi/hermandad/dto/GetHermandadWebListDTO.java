4815c81b36160bfe38fe98757782bc1f3e0f1da0
Antonio Martinez <nonomartinezlopez2004@gmail.com>
2024-06-03 22:28:35 +0200

    funcionalidad agregada correctamente

---
+++ api/CofradiasAPI/src/main/java/com/nonomartinez/sfc/cofradiasapi/hermandad/dto/GetHermandadWebListDTO.java
@@ -0,0 +1,26 @@
+package com.nonomartinez.sfc.cofradiasapi.hermandad.dto;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import com.nonomartinez.sfc.cofradiasapi.hermandad.model.Hermandad;
+
+import java.util.UUID;
+
+public record GetHermandadWebListDTO(
+        UUID id,
+        String nombre,
+        String dia,
+        int numPasos,
+        int numNazarenos,
+        int numHnos
+) {
+    public static GetHermandadWebListDTO of(Hermandad hermandad){
+        return new GetHermandadWebListDTO(
+                hermandad.getId(),
+                hermandad.getNombre(),
+                hermandad.getDiaSalida().toString(),
+                hermandad.getPasos().size(),
+                hermandad.getNumNazarenos(),
+                hermandad.getNumHermanos()
+        );
+    }
+}
